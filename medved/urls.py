b6e4501a6db7ef44885fb04549409013d4e72698
Pylearn2017 <alexlik73@gmail.com>
2019-06-29 20:44:31 +0300

    reg urls.py

--- medved/urls.py
+++ medved/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from users import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reg/', views.register, name = 'reg'),
+    path('reg/', include('users.urls')),
     path('', include('home.urls')),
 ]
